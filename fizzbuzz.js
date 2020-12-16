a5363ee083698d34a14318c9e3eb828cee559804
Vaughn McPherson <Vaughnsmcp@gmail.com>
2020-12-16 13:02:08 -0800

    Finished, pushed from HD

---
+++ fizzbuzz.js
@@ -0,0 +1,24 @@
+function FizzBuzz(value1, value2) {
+  let returnValue = "";
+  for (let i = 1; i <= 100; i++) {
+    if (i % value1 == 0 && i % value2 == 0) {
+      returnValue += "FizzBuzz ";
+    } else if (i % value1 == 0) {
+      returnValue += "Fizz ";
+    } else if (i % value2 == 0) {
+      returnValue += "Buzz ";
+    } else {
+      returnValue += i + " ";
+    }
+  }
+  return returnValue;
+}
+
+function buzzIt() {
+  let output = "";
+  let val1 = document.getElementById("Fizzvalue").value;
+  let val2 = document.getElementById("Buzzvalue").value;
+  output = FizzBuzz(val1, val2);
+  document.getElementById("results").innerHTML = output;
+}
+
